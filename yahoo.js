99b729bdb1f4853d53b4a671ec2d7c02c592a3e5
Chris Campbell <chris@harto.org>
2012-03-05 14:14:01 +1100

    Namespaced yahoo functions

--- yahoo.js
+++ yahoo.js
@@ -1,30 +1,30 @@
 var http = require('http'),
-		xml2js = require('xml2js'),
-		request = require('weather');
+	xml2js = require('xml2js'),
+	request = require('weather');
+
+module.exports = function(geo, callback) {
+	yahoo.where(geo, function(woeid) {
+		yahoo.weather(woeid, callback);
+	});
+};
+
+var yahoo = {};
 
 /**
  * Get the Yahoo weather based on geolocation.
  */
-module.exports = function(geo, callback) {
-	where(geo, function(woeid) {
-		var options = {
-				host: 'weather.yahooapis.com',
-				path: '/forecastrss?w='+woeid+'&u=c'
-		};
+yahoo.weather = function(woeid, callback) {
+	var options = {
+		host: 'weather.yahooapis.com',
+		path: '/forecastrss?w='+woeid+'&u=c'
+	};
 
-		http.get(options, function(res) {
-			var data = '';
-			res.on("data", function(chunk) {
-				data += chunk;
-			});
-
-			res.on('end', function() {
-				var parser = new xml2js.Parser();
-				parser.parseString(data, function (err, result) {
-					var temp = result.channel.item['yweather:condition']['@'].temp;
-					callback(temp);
-				});
-			});
+	var url = 'http://weather.yahooapis.com/forecastrss?w='+woeid+'&u=c';
+	request(url, function(error, res, body) {
+		var parser = new xml2js.Parser();
+		parser.parseString(data, function (err, result) {
+			var temp = result.channel.item['yweather:condition']['@'].temp;
+			callback(temp);
 		});
 	});
 };
@@ -32,10 +32,10 @@ module.exports = function(geo, callback) {
 /**
  * Get yahoo location base on geolocation.
  */
-where = function(geo, callback) {
+yahoo.where = function(geo, callback) {
 	var options = {
-			host: 'where.yahooapis.com',
-			path: '/geocode?location='+geo.lat+','+geo.long+'&flags=J&gflags=R'
+		host: 'where.yahooapis.com',
+		path: '/geocode?location='+geo.lat+','+geo.long+'&flags=J&gflags=R'
 	};
 
 	http.get(options, function(res) {
