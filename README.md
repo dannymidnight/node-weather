b949db22f5af3ebe435b77c16b41109e6187146d
Chris Campbell <chris@harto.org>
2013-08-03 20:38:01 +1000

    Use "place" rather than lat/long to determine weather

--- README.md
+++ README.md
@@ -1,21 +1,23 @@
-An example of how to get the weather based on geolocation.
+## Example
+An example of how to get the weather based on location.
 
 ```javascript
  var weather = require('weather');
 
- weather({lat: -50.212, long: 10.829}, function(data) {
-	if (data.temp > 30)
-		console.log("Damn it's hot!");
+ weather({location: 'Melbourne'}, function(data) {
+   if (data.temp > 30) {
+     console.log("Damn it's hot!");
+   }
  });
  ```
- 
+
  Example JSON result:
- 
+
 ```javascript
  {
- 	temp: 18,	// Current temperature
- 	high: 20,	// High for the day
- 	low: 9,		// Low for the day
+  temp: 18, // Current temperature
+  high: 20, // High for the day
+  low: 9,   // Low for the day
  }
 ```
 
--- yahoo.js
+++ yahoo.js
@@ -1,74 +1,95 @@
 var xml2js = require('xml2js'),
-		request = require('request');
+    request = require('request'),
+    printf = require('util').format;
 
-module.exports = function(geo, callback) {
-	yahoo.logging = geo.logging;
-
-	yahoo.where(geo, function(woeid) {
-		yahoo.weather(woeid, callback);
-	});
+module.exports = function(options, callback) {
+  yahoo.logging = options.logging;
+  yahoo.where(options, function(woeid) {
+    yahoo.weather(woeid, callback);
+  });
 };
 
 /**
  * Report error.
  */
 var onError = function(msg) {
-	if (module.exports.error) {
-		module.exports.error(msg);
-	}
+  if (yahoo.logging) {
+    console.log('ERROR: ' + msg);
+  }
+
+  if (module.exports.error) {
+    module.exports.error(msg);
+  }
 };
 
 var yahoo = {
-	logging: false
+  logging: false
 };
 
 /**
  * Get the Yahoo weather based on geolocation.
  */
 yahoo.weather = function(woeid, callback) {
-	var url = 'http://weather.yahooapis.com/forecastrss?w='+woeid+'&u=c';
+  var url = 'http://weather.yahooapis.com/forecastrss?w='+woeid+'&u=c';
 
-	if (yahoo.logging)
-		console.log('Requesting %s', url);
+  if (yahoo.logging) {
+    console.log('Requesting %s', url);
+  }
 
-	request(url, function(error, res, body) {
-		var parser = new xml2js.Parser();
-		parser.parseString(body, function (err, result) {
-			var weather = {},
-				high = /(?:High:\s)(-?\d+)/g,
-				low = /(?:Low:\s)(-?\d+)/g;
+  request(url, function(error, res, body) {
+    var parser = new xml2js.Parser();
+    parser.parseString(body, function (err, result) {
+      var weather = {},
+          high = /(?:High:\s)(-?\d+)/g,
+          low = /(?:Low:\s)(-?\d+)/g;
 
-			try {
-				// Get the current temp
-				var condition = result.channel.item['yweather:condition']['@'];
-				weather = condition;
+      try {
+        // Get the current temp
+        var condition = result.channel.item['yweather:condition']['@'];
+        weather = condition;
 
-				// Get the high/low currently stored in Yahoo widget.
-				var description = result.channel.item['description'];
-				weather.high = high.exec(description)[1];
-				weather.low = low.exec(description)[1];
-			} catch(e) {
-				onError('Failed to find weather');
-				return;
-			}
+        // Get the high/low currently stored in Yahoo widget.
+        var description = result.channel.item['description'];
+        weather.high = high.exec(description)[1];
+        weather.low = low.exec(description)[1];
+      } catch(e) {
+        onError('Failed to find weather');
+        return;
+      }
 
-			callback(weather);
-		});
-	});
+      callback(weather);
+    });
+  });
 };
 
 /**
  * Get yahoo location base on geolocation.
  */
-yahoo.where = function(geo, callback) {
-	var url = 'http://where.yahooapis.com/geocode?location='+geo.lat+','+geo.long+'&flags=J&gflags=R';
-	
-	if (yahoo.logging)
-		console.log('Requesting %s', url);
+yahoo.where = function(options, callback) {
+  var url = printf(
+    "http://where.yahooapis.com/v1/places.q(\"%s\")?appid=%s",
+    options.location,
+    options.appid
+  );
 
-	request({url:url, json:true}, function(error, res, body) {
-		var result = body.ResultSet;
-		if (result.Found)
-			callback(result.Results[0].woeid);
-	});
+  if (yahoo.logging) {
+    console.log('Requesting %s', url);
+  }
+
+  request(url, function(error, res, body) {
+    var parser = new xml2js.Parser();
+
+    if (body) {
+      parser.parseString(body, function (err, result) {
+        try {
+          var woeid = result.place.woeid;
+          callback(woeid);
+        } catch(e) {
+          onError('Failed to fetch woeid');
+        }
+      });
+    } else {
+      onError(error);
+    }
+  });
 };
